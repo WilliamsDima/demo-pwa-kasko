fd1b745460b52a7e53a8decfbacc5e633971eec8
Dmitry <72758000+WilliamsDima@users.noreply.github.com>
2026-08-18 09:54:50 +0300

    Update inspection.effects.ts

--- src/app/inspection/state/inspection.effects.ts
+++ src/app/inspection/state/inspection.effects.ts
@@ -1,14 +1,35 @@
 import { inject, Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
-import { catchError, exhaustMap, filter, from, interval, map, of, takeUntil, tap, withLatestFrom } from 'rxjs';
+import {
+  catchError,
+  exhaustMap,
+  filter,
+  from,
+  interval,
+  map,
+  of,
+  takeUntil,
+  tap,
+  withLatestFrom,
+} from 'rxjs';
 import { CameraService } from '../../core/services/camera.service';
 import { ConnectivityService } from '../../core/services/connectivity.service';
 import { InspectionUploadService } from '../../core/services/inspection-upload.service';
 import { CameraAccessError } from '../../core/models/camera.model';
 import { CapturedPhoto } from '../../core/models/capture-photo.model';
-import { cameraActions, connectivityActions, sessionActions, submissionActions, timerActions } from './inspection.actions';
-import { selectCapturedPhotosList, selectRemainingSeconds, selectVehicle } from './inspection.selectors';
+import {
+  cameraActions,
+  connectivityActions,
+  sessionActions,
+  submissionActions,
+  timerActions,
+} from './inspection.actions';
+import {
+  selectCapturedPhotosList,
+  selectRemainingSeconds,
+  selectVehicle,
+} from './inspection.selectors';
 
 @Injectable()
 export class InspectionEffects {
@@ -24,10 +45,10 @@ export class InspectionEffects {
       exhaustMap(() =>
         interval(1000).pipe(
           map(() => timerActions.tick({ nowMs: Date.now() })),
-          takeUntil(this.actions$.pipe(ofType(sessionActions.reset, timerActions.expired))),
-        ),
-      ),
-    ),
+          takeUntil(this.actions$.pipe(ofType(sessionActions.reset, timerActions.expired)))
+        )
+      )
+    )
   );
 
   readonly expireTimer$ = createEffect(() =>
@@ -35,8 +56,8 @@ export class InspectionEffects {
       ofType(timerActions.tick),
       withLatestFrom(this.store.select(selectRemainingSeconds)),
       filter(([, remainingSeconds]) => remainingSeconds <= 0),
-      map(() => timerActions.expired()),
-    ),
+      map(() => timerActions.expired())
+    )
   );
 
   readonly requestCameraAccess$ = createEffect(() =>
@@ -45,10 +66,12 @@ export class InspectionEffects {
       exhaustMap(() =>
         from(this.cameraService.requestAccess()).pipe(
           map(() => cameraActions.accessGranted()),
-          catchError((error: unknown) => of(cameraActions.accessDenied({ error: error as CameraAccessError }))),
-        ),
-      ),
-    ),
+          catchError((error: unknown) =>
+            of(cameraActions.accessDenied({ error: error as CameraAccessError }))
+          )
+        )
+      )
+    )
   );
 
   readonly applyTorch$ = createEffect(
@@ -57,41 +80,49 @@ export class InspectionEffects {
         ofType(cameraActions.torchToggled),
         tap(({ enabled }) => {
           void this.cameraService.setTorch(enabled);
-        }),
+        })
       ),
-    { dispatch: false },
+    { dispatch: false }
   );
 
   readonly stopCameraOnReset$ = createEffect(
     () =>
       this.actions$.pipe(
         ofType(sessionActions.reset),
-        tap(() => this.cameraService.stop()),
+        tap(() => this.cameraService.stop())
       ),
-    { dispatch: false },
+    { dispatch: false }
   );
 
   readonly watchConnectivity$ = createEffect(() =>
-    this.connectivityService.online$.pipe(map((online) => connectivityActions.statusChanged({ online }))),
+    this.connectivityService.online$.pipe(
+      map((online) => connectivityActions.statusChanged({ online }))
+    )
   );
 
   readonly submit$ = createEffect(() =>
     this.actions$.pipe(
       ofType(submissionActions.submitRequested),
       withLatestFrom(this.store.select(selectCapturedPhotosList), this.store.select(selectVehicle)),
-      exhaustMap(([, photosList, vehicle]) => {
+      exhaustMap(([action, photosList, vehicle]) => {
         const photos = photosList.filter((photo): photo is CapturedPhoto => photo !== null);
         if (!vehicle) {
           return of(
             submissionActions.eventReceived({
               event: { phase: 'error', message: 'Не удалось определить автомобиль осмотра' },
-            }),
+            })
           );
         }
-        return this.uploadService.upload(photos, vehicle).pipe(
-          map((event) => submissionActions.eventReceived({ event })),
-        );
-      }),
-    ),
+
+        if (action.isMock) {
+          return this.uploadService
+            .mockUpload(photos, vehicle)
+            .pipe(map((event) => submissionActions.eventReceived({ event })));
+        }
+        return this.uploadService
+          .upload(photos, vehicle)
+          .pipe(map((event) => submissionActions.eventReceived({ event })));
+      })
+    )
   );
 }
